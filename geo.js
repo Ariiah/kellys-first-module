34362883ac31faf48c419d07472a60f0bf3ece72
Kelly L <lock2846@gmail.com>
2018-03-19 11:37:40 -0600

    Dependency stuff

--- geo.js
+++ geo.js
@@ -1,5 +1,6 @@
 const { spherev, torusv } = require('./vol')
+const chalk = require('chalk')
 
-console.log(torusv(4, 3));
+console.log(chalk.yellow(torusv(4, 3)));
 
-console.log(spherev(1));
+console.log(chalk.bgBlue(chalk.black(spherev(1))));
